8e40d527d4747f82be70ab9b1606b8ff6f916397
SteffYakim <andry2092@gmail.com>
2026-08-08 16:05:46 +0300

    Create RootLayout component with metadata

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Gestion de Stock',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    
+      {children}
+    
+  )
+}
